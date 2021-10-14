61836c956f83b51a0082055eee2b2fef4526923d
Igor Koury <87314880+igorkoury@users.noreply.github.com>
2021-10-14 18:21:26 -0300

    Add files via upload

---
+++ 12-forEach- media-array.js
@@ -0,0 +1,11 @@
+const notas = [10, 6.5, 8, 7.5]
+
+let somaDasNotas = 0
+
+notas.forEach(nota => {
+    somaDasNotas += nota
+})
+
+const media = somaDasNotas / notas.length
+
+console.log(media)
---
+++ 13-callbacks-em-arrays.js
@@ -0,0 +1,7 @@
+const nomes = ['Igor', 'Fernanda', 'Thiago', 'Valdir', 'Rodrigo']
+
+nomes.forEach(imprimeNomes) //imprime todos os nomes do array
+
+function imprimeNomes(nome) {
+    console.log(nome)
+}
